2c18f80c4968b60662e6d9fbdb4e87dae2d8e8f6
Aryan76 <19621585+Aryan76@users.noreply.github.com>
2019-03-01 15:26:47 +0530

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -97,7 +97,8 @@ if (window.self === window.top) {
 		function saveText() {
 			var html = quill.root.innerHTML;
 			sdk.setContent(html);
-			
+			sdk.setSuperContent('This is super content: ' + html);
+
 			sdk.getData(function (data) {
 				var numberOfEdits = data.numberOfEdits || 0;
 				sdk.setData({
