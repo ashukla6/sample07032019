2b31000fab00d575313a7f607b8359da032d3cc3
Aryan76 <19621585+Aryan76@users.noreply.github.com>
2019-03-04 16:36:43 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,36 @@ require('../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-l
 var SDK = require('blocksdk');
 var sdk = new SDK();
 
+function debounce (func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
 
-	sdk.getContent(function (content) {
-		var quill = new Quill('#editor-container', {
-			theme: 'snow'
-		});
-		quill.root.innerHTML = content;
+function save()
+{
+	var fieldvalue= document.getElementById('textfield').value;
+localStorage.setItem('text',fieldvalue);
+	
+}
+
+function load()
+{
+var storedvalue= localStorage.getItem('text');
+}
+var fieldvalue= document.getElementById('textfield').value;
+sdk.getContent(function save() {
+		var quill = new Quill('textfield'});
+		quill.root.innerHTML = fieldvalue;
 		function saveText() {
 			var html = quill.root.innerHTML;
 			sdk.setContent(html);
